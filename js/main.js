ccf41892a98e67675b412183e1b358fbcf7a3155
unknown <anan.daraghmeh@yahoo.com>
2019-01-15 22:44:27 +0100

    update service worker

--- js/main.js
+++ js/main.js
@@ -8,8 +8,16 @@ $('#toggler').on('click', (e)=>{
 })
 
 // service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../sw.js');
+if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('../sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
   }
+  
 
 });
