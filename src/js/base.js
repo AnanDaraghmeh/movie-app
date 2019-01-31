7927f5ef4754ca16b84d1d086bf1ff24ef69f79c
unknown <anan.daraghmeh@yahoo.com>
2019-01-31 22:35:11 +0100

    change sidemenu style

--- src/js/base.js
+++ src/js/base.js
@@ -3,14 +3,21 @@ import $ from 'jquery';
 //toggler
 $('#toggler').on('click', (e)=>{
     $('#nav-menu').toggleClass('js-open');
-    $('body').toggleClass('js-moveToRight');
     $('.brand').toggleClass('js-invisible');
-})
+});
+
+//closing the side menu
+$('html').on('click', (e)=>{
+    if (!e.target.classList.contains('toggler') && !e.target.classList.contains('sideMenu') && !e.target.classList.contains('sideMenu_link')){
+        $('.sideMenu').removeClass('js-open');
+        $('.brand').toggleClass('js-invisible');
+    }
+});
 
 // spinner
 $(window).on('load', ()=>{
     $('.spinner').addClass('js-invisible');
     $('.spinner').on('transitionend', (e)=>{
         $('.spinner').css({'display':'none'})
-    })
-})
+    });
+});
--- src/js/modules/popMovieCard.js
+++ src/js/modules/popMovieCard.js
@@ -1,6 +1,6 @@
 function createPopMovieCard (movie){
     return `<div class="popMovieCard" data-id="${movie.id}">
-                <div class="popMovieCard_imageWrapper"><img class="popMovieCard_image" src="https://image.tmdb.org/t/p/original/${movie.poster_path}"></div>
+                <div class="popMovieCard_imageWrapper"><img class="popMovieCard_image" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"></div>
                 <div class="popMovieCard_overlay">
                     <h2>${movie.title}</h2>
                     <p>Released in ${movie.release_date.substring(0, 4)}</p>
